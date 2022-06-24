c98ee7ceeaab131b51d6ac496e783e72e918b986
Muhammad Ansori Nasution <muhammadansorinasution@gmail.com>
2022-06-24 09:22:22 +0700

    a

--- App.js
+++ App.js
@@ -5,6 +5,51 @@ import {Provider} from 'react-redux';
 import store from './src/redux/store';
 
 export default function App() {
+  function hitung() {
+    var list = [];
+    const listAlpa = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+    ];
+
+    var split = 'saya makan nasi goreng'.split('');
+    listAlpa.forEach(element => {
+      var jumlah = 0;
+      split.forEach(data => {
+        if (element == data) {
+          jumlah++;
+        }
+      });
+      if (jumlah > 0) {
+        list.push({[element]: jumlah});
+      }
+    });
+    console.log(list);
+  }
   return (
     <Provider store={store}>
       <Routers />
